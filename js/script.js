39a8a62bfdd04babc329a26938e9149f7cd720c3
Sarah Weisser <sarahweisser@zipcoder23.local>
2017-05-26 10:45:57 -0400

    made function to combine arrays in alternating order

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 " use strict ";
 
 var display = document.getElementById("display");
+var array1 = [];
+var array2 = [];
+
+var arr1 = ["a", "b", "c", "d"];
+var arr2 = [1, 2, 3, 4];
 
 function yourFunctionName (){
   display.innerHTML = "hello";
@@ -8,6 +13,19 @@ function yourFunctionName (){
 
 function example(){
   var v = 3 +4;
-  
+
   display.innerHTML = v;
+}
+
+function combineArrays(array1, array2) {
+  var length;
+  var combinedArray = [];
+  if (array1.length <= array2.length) {
+    length = array2.length;
+  }
+  for (var i = 0; i < length; i++) {
+    combinedArray.push(array1[i]);
+    combinedArray.push(array2[i]);
+  }
+  display.innerHTML = combinedArray.toString();
 }
